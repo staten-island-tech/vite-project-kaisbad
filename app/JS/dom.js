21d0041ebb80bf442c79a27f12f72eeb50006ff4
kaisbad7 <158204041+kaisbad@users.noreply.github.com>
2024-11-12 22:00:03 -0500

    a

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,6 +1,13 @@
 const DOMSelectors = {
   container: document.querySelector(".container"),
   button: document.querySelector(".btn"),
+  atk: document.getElementById("atk"),
+  def: document.getElementById("def"),
+  male: document.getElementById("male"),
+  female: document.getElementById("female"),
+  all: document.getElementById("all"),
+  warm: document.getElementById("warm"),
+  cool: document.getElementById("cool"),
 };
 
 export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -2,27 +2,72 @@ import "../CSS/style.css";
 import { DOMSelectors } from "../JS/dom.js";
 import { operators } from "../JS/product.js";
 
-console.log(operators.includes("Defender"));
-let cardHtml = "";
-
-operators.forEach((operator) => {
-  cardHtml += `<div class="card"><h2 class="header">${operator.name}</h2>
-      <ul><li>${operator.gender}</li>
-      <li>${operator.side}</li>
-      <li>${operator.price}</li></ul>
-      <img src="${operator.image}" class="card-img"></div>
-      `;
-});
-
-function insertCard() {
+function insertCard(operators) {
+  clearCards();
+  let cardHtml = "";
+  operators.forEach((operator) => {
+    cardHtml += `<div class="card"><h2 class="header">${operator.name}</h2>
+        <ul><li>${operator.gender}</li>
+        <li>${operator.side}</li>
+        <li>${operator.price}</li></ul>
+        <img src="${operator.image}" class="card-img"></div>
+        `;
+  });
   DOMSelectors.container.insertAdjacentHTML("afterbegin", cardHtml);
 }
-window.onload = insertCard();
 
-/* const sortDefenders = console.log(
-  operators.foreach((operator) =>
-    operator.includes((operators.side = "Defender"))
-  )
-);
+function clearCards() {
+  DOMSelectors.container.innerHTML = "";
+}
 
-console.log(sortDefenders); */
+DOMSelectors.all.addEventListener("click", () => {
+  clearCards();
+  insertCard(operators);
+});
+
+DOMSelectors.atk.addEventListener("click", () => {
+  const sortAttackers = operators.filter(
+    (operator) => operator.side == "Attacker"
+  );
+  clearCards();
+  insertCard(sortAttackers);
+});
+DOMSelectors.def.addEventListener("click", () => {
+  const sortDefenders = operators.filter(
+    (operator) => operator.side == "Defender"
+  );
+  clearCards();
+  insertCard(sortDefenders);
+});
+
+DOMSelectors.male.addEventListener("click", () => {
+  const sortMale = operators
+    .map((operator) => (operator.gender == "Male" ? operator : null))
+    .filter((operator) => operator != null);
+  clearCards();
+  insertCard(sortMale);
+});
+
+DOMSelectors.female.addEventListener("click", () => {
+  const sortFemale = operators
+    .map((operator) => (operator.gender == "Female" ? operator : null))
+    .filter((operator) => operator != null);
+  clearCards();
+  insertCard(sortFemale);
+});
+
+function clearColors() {
+  document.body.classList.remove("warm", "cool");
+}
+
+DOMSelectors.warm.addEventListener("click", () => {
+  clearColors();
+  document.body.classList.add("warm");
+});
+
+DOMSelectors.cool.addEventListener("click", () => {
+  clearColors();
+  document.body.classList.add("cool");
+});
+
+insertCard(operators);
